1cf69e37770528d110f76e640ba78a6826ab9934
LMO GerritBot <gerrit@libremobileos.com>
2024-05-31 04:09:18 +0000

    Adapt to LMODroid by device-add script

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     device/essential/mata/overlay \
-    device/essential/mata/overlay-lineage
+    device/essential/mata/overlay-lmodroid
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # AAPT
